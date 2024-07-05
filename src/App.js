6f3b4ef433980ac096e8bd0ed75c790741bad5c5
21701A05E0 <rraj6170825@gmail.com>
2024-07-05 12:02:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import ExpenseEntryItem from "./ExpenseEntryItem";
+import Registration from "./Registration";
+
+function App() {
+  return (
+    <div ClassName="App">
+      <ExpenseEntryItem />
+      <Registration />
+    </div>
+  );
+}
+
+export default App;
